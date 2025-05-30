2950929d031a99891b63c3e645657db2ca24211e
marrinda <maria.arrind@gmail.com>
2025-05-30 11:29:48 -0300

    changed switch case to array of funtion pointers

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -7,15 +7,21 @@
 static void int_20();
 static void int_21();
 
+static void (*functions[])() = {
+    &int_20, // IRQ 0
+    &int_21 // IRQ 1
+};
+
 void irqDispatcher(uint64_t irq) {
-  switch (irq) {
-  case 0:
-    int_20();
-    break;
-  case 1:
-    int_21();
-    break;
-  }
+  functions[irq]();
+  // switch (irq) {
+  // case 0:
+  //   int_20();
+  //   break;
+  // case 1:
+  //   int_21();
+  //   break;
+  // }
 
   return;
 }
