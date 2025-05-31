713ef1f98ab9b5ba24cc89d9ff02ae9dd79910e3
acragno <acragno@itba.edu.ar>
2025-05-31 20:51:28 -0300

    added flags to run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+# flags -audiodev pa,id=speaker -machine pcspk-audiodev=speaker usadas para sonido
+# flag -rtc base=localtime agregada para que se ajuste el horario
+qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker -rtc base=localtime
