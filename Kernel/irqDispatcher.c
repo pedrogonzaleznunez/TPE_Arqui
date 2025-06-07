09e297bca76afe64500fb36336ff8ae5613f6ffa
acragno <acragno@itba.edu.ar>
2025-06-07 13:04:35 -0300

    videoDriver optimizado con memset y memcopy

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -13,16 +13,14 @@ static void (*functions[])() = {
 };
 
 void irqDispatcher(uint64_t irq) {
-  functions[irq]();
-  return;
-
+    functions[irq]();
+    return;
 }
 
 static void int_20() {
     timer_handler();
 }
 
-static void int_21() { 
-  keyboardHandler(); 
- }
-
+static void int_21() {
+    keyboardHandler();
+}
